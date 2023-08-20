fea3bc1841f4f613c67806676a613b32a56df47a
Anatoly61 <128617494+Anatoly61@users.noreply.github.com>
2023-08-20 22:07:16 +0300

    Изменения от 20.08.2023

--- public/js/ui/widgets/UserWidget.js
+++ public/js/ui/widgets/UserWidget.js
@@ -12,7 +12,11 @@ class UserWidget {
    * необходимо выкинуть ошибку.
    * */
   constructor(element){
+    if (!element) {
+      throw new Error('Element was not found');
+    }
 
+     this.element = element;
   }
 
   /**
@@ -23,6 +27,13 @@ class UserWidget {
    * авторизованного пользователя
    * */
   update(){
+    const currentUser = User.current();
 
+    if (!currentUser) {
+        return;
+    }
+
+    const userName = this.element.querySelector('.user-name');
+    userName.textContent = currentUser.name;
   }
 }
